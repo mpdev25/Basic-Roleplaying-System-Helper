bc7a558347b8b9c77ad69f38ae66ef3b79db325f
mpdev25 <michaelpeake123@gmail.com>
2026-08-19 07:14:40 -0700

    Starting onn structure

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
-
+import (
+	"http/template"
+	"log"
+	"net/http"
+)
 var tmpl *template.Template
 
 func main(){
@@ -7,5 +11,13 @@ func main(){
 	tmpl, err = template.ParseFiles("templates/index.html")
 	if err != nill {
 		log.Fatalf("Error parsing template: %v", err)
+	}
+
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/generate", handleGenerate)
+
+	log.Println("Server running at http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
 }
